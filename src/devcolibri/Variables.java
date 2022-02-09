f225027d26ebaf17bba12c7ecee07a159c637a0a
Work <spirit40web@gmail.com>
2022-02-09 10:11:26 +0300

    add file Variables

---
+++ src/devcolibri/Variables.java
@@ -0,0 +1,23 @@
+package devcolibri;
+
+public class Variables {
+
+    static byte b = 1;
+    static short s = 5;
+    static int i = 10;
+    static float f = 11.1f;
+    static double d = 22.2;
+    static boolean bt = true;
+    static boolean bf = false;
+
+    public static void main(String[] args) {
+
+        System.out.println("byte: " + b + "\n" +
+                "short: " + s + "\n" +
+                "int: " + i + "\n" +
+                "float: " + f + "\n" +
+                "double: " + d + "\n" +
+                "boolean true: " + bt + "\n" +
+                "boolean false: " + bf);
+    }
+}
