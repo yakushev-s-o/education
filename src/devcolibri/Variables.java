be35df91718c58930a928ff353f162e16ea7052b
Work <spirit40web@gmail.com>
2022-02-09 10:45:45 +0300

    update class Variables

--- src/devcolibri/Variables.java
+++ src/devcolibri/Variables.java
@@ -2,11 +2,17 @@ package devcolibri;
 
 public class Variables {
 
-    static byte b = 1;
-    static short s = 5;
-    static int i = 10;
-    static float f = 11.1f;
-    static double d = 22.2;
+    /*
+    Примитивные типы данных
+     */
+
+    static byte b = 127; // -128 -> 127;
+    static short s = 32767; // -32768 -> 32767
+    static int i = 2147483647; // -2147483648 -> 2147483647
+    static long l = 9223372036854775807L; // -9223372036854775808 до 9223372036854775807
+    static float f = 3.4E38f; // -3.4E+38 -> 3.4E+38
+    static double d = 1.7E308; // -1.7E+308 -> 1.7E+308
+    static char c = 'c'; // c = 99
     static boolean bt = true;
     static boolean bf = false;
 
@@ -15,8 +21,10 @@ public class Variables {
         System.out.println("byte: " + b + "\n" +
                 "short: " + s + "\n" +
                 "int: " + i + "\n" +
+                "long: " + l + "\n" +
                 "float: " + f + "\n" +
                 "double: " + d + "\n" +
+                "char: " + c + "\n" +
                 "boolean true: " + bt + "\n" +
                 "boolean false: " + bf);
     }
