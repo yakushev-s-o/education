58f560b61cc10342985ad0f298eb0c7117a02f36
Home <spirit40web@gmail.com>
2022-08-14 21:31:25 +0300

    add new topic thinkingInJava, class HelloDate

---
+++ src/thinkingInJava/HelloDate.java
@@ -0,0 +1,19 @@
+package thinkingInJava;
+
+import java.util.*;
+
+/** Первая программа.
+ * Выводит строку и текущее число.
+ * @author Брюс Эккель
+ * @author www.site.ru
+ * @version 1.0.0.0
+ */
+public class HelloDate {
+    /** Точка входа в класс и приложение.
+     * @param args Массив строковых агрументов.
+     */
+    public static void main(String[] args) {
+        System.out.println("Привет, сегодня: ");
+        System.out.println(new Date());
+    }
+}
