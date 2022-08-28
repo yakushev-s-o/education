8c81cecae17ac3c0ecfcd894bbb408f1efde710e
Home <spirit40web@gmail.com>
2022-08-28 18:07:31 +0300

    add class E5_BitwiseTest

---
+++ src/thinkingInJava/control/E5_BitwiseTest.java
@@ -0,0 +1,33 @@
+package thinkingInJava.control;
+
+public class E5_BitwiseTest {
+    private static void binaryPrint (int q) {
+        if (q == 0) {
+            System.out.print("0");
+        } else {
+            int n = Integer.numberOfLeadingZeros(q);
+            q <<= n;
+            for(int p = 0; p < Integer.SIZE - n; p++) {
+                System.out.print((Integer.numberOfLeadingZeros(q) == 0) ? 1 : 0);
+                q <<= 1;
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int low = 0xAA;
+        int msb = 0xDD; // most significant bit
+
+        System.out.print("low - ");
+        binaryPrint(low);
+        System.out.print("msb - ");
+        binaryPrint(msb);
+        System.out.print("low&msb - ");
+        binaryPrint(low&msb);
+        System.out.print("low|msb - ");
+        binaryPrint(low|msb);
+        System.out.print("~low - ");
+        binaryPrint(~low);
+    }
+}
