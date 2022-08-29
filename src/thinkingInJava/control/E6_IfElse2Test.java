f867597290aaa84fcbb1c50a7e7789fb9b11ea25
Home <spirit40web@gmail.com>
2022-08-29 20:15:32 +0300

    add class E6_IfElse2Test

---
+++ src/thinkingInJava/control/E6_IfElse2Test.java
@@ -0,0 +1,31 @@
+package thinkingInJava.control;
+
+import static thinkingInJava.util.Print.*;
+
+public class E6_IfElse2Test {
+    static int test(int testval, int begin, int end) {
+        for(int i = begin; i <=end; i++) {
+            if(testval == i) {
+                printnb("true ");
+                return 1;
+            }
+        }
+        printnb("false ");
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        print(test(15, 10, 20));
+        print(test(10, 10, 20));
+        print(test(20, 10, 20));
+        print(test(9, 10, 20));
+        print(test(21, 10, 20));
+    }
+}
+
+//    static boolean test(int testval, int begin, int end) {
+//        if(testval >= begin && testval <= end)
+//            return true;
+//        else
+//            return false;
+//    }
