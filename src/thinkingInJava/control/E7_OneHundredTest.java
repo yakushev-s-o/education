28bbd3a2e8b8a18484846ca608dc083f9b37ce4f
Home <spirit40web@gmail.com>
2022-08-29 20:40:24 +0300

    add class E7_OneHundredTest

---
+++ src/thinkingInJava/control/E7_OneHundredTest.java
@@ -0,0 +1,20 @@
+package thinkingInJava.control;
+
+public class E7_OneHundredTest {
+    static int whileCycle(){
+        int i = 0;
+        int stop = 99;
+        while (i <= 100) {
+            System.out.print(i + " ");
+            i++;
+            if(i == stop) {
+                break;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(whileCycle());
+    }
+}
