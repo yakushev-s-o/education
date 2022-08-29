85395ace81504d997a9ae190a11468e1914432b9
Home <spirit40web@gmail.com>
2022-08-29 20:58:17 +0300

    add class E8_SwitchTest

---
+++ src/thinkingInJava/control/E8_SwitchTest.java
@@ -0,0 +1,42 @@
+package thinkingInJava.control;
+
+import static thinkingInJava.util.Print.*;
+
+public class E8_SwitchTest {
+    static void testBreak() {
+        for (int i = 0; i < 5; i++) {
+            switch(i) {
+                case 0: printnb("0 ");
+                    break;
+                case 1: printnb("1 ");
+                    break;
+                case 2: printnb("2 ");
+                    break;
+                case 3: printnb("3 ");
+                    break;
+                case 4: printnb("4 ");
+                    break;
+                default: printnb("END");
+            }
+        }
+    }
+
+    static void testWithoutBreak(){
+        for (int i = 0; i < 5; i++) {
+            switch(i) {
+                case 0: printnb("0 ");
+                case 1: printnb("1 ");
+                case 2: printnb("2 ");
+                case 3: printnb("3 ");
+                case 4: printnb("4 ");
+                default: printnb("END ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        testBreak();
+        print();
+        testWithoutBreak();
+    }
+}
