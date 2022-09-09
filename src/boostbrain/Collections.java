7058facb845496944cdb816cb0afb2293ef54510
Home <spirit40web@gmail.com>
2022-09-09 21:21:44 +0300

    update class Collections

--- src/boostbrain/Collections.java
+++ src/boostbrain/Collections.java
@@ -3,10 +3,11 @@ package boostbrain;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.TreeMap;
 
 public class Collections {
     public static void main(String[] args) throws IOException {
+        // Считать данные csv файла и поместить в String
         FileInputStream stream = new FileInputStream("src/files/database.csv");
         int length = stream.available();
         byte [] data = new byte[length];
@@ -14,6 +15,7 @@ public class Collections {
         String text = new String(data);
 //        System.out.println(text);
 
+        // Разделить строки поместив в ArrayList, затем разделить строки на слова поместив в String []
         ArrayList <String []> lineWords = new ArrayList<>();
         String [] lines = text.split("\n");
         for(String line : lines){
@@ -22,6 +24,7 @@ public class Collections {
             lineWords.add(words);
         }
 
+        // Вывести в консоль форматированные строки по ключевому слову
         for(String [] words: lineWords){
             if(words[5].equals("бар")){
                 for(String word : words){
@@ -32,7 +35,8 @@ public class Collections {
             }
         }
 
-        HashMap<String, Integer> map = new HashMap<>();
+        // Вывести в консоль коливество строк по ключевому слову
+        TreeMap<String, Integer> map = new TreeMap<>(); // HashMap без сортировки
         for (String [] words: lineWords) {
             if (map.containsKey(words[5])){
                 Integer count = map.get(words[5]);
@@ -42,6 +46,11 @@ public class Collections {
                 map.put(words[5], 1);
             }
         }
-        System.out.println("Итого: " + map.get("бар"));
+//        System.out.println("Итого: " + map.get("бар"));
+
+        // Вывести в консоль количество для каждого ключевого слова
+        for (String key : map.keySet()) {
+            System.out.println("Тип: " + key + " -> Количество: " + map.get(key));
+        }
     }
 }
