2bbe0241461174f655c91481b1fa0c5c93d4ee3d
Home <spirit40web@gmail.com>
2023-01-12 22:07:42 +0300

    update cinema_room_manager/Main.java

--- src/hyperskill/cinema_room_manager/Main.java
+++ src/hyperskill/cinema_room_manager/Main.java
@@ -1,6 +1,6 @@
 package hyperskill.cinema_room_manager;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     private static final char SEAT = 'S';
@@ -17,7 +17,7 @@ public class Main {
 
     public static Scanner sc = new Scanner(System.in);
 
-    public static void clearSeats() {
+    public static void clearCinema() {
         for (int i = 1; i < cinema.length; i++) {
             for (int j = 1; j < cinema[i].length; j++) {
                 cinema[i][j] = SEAT;
@@ -28,7 +28,7 @@ public class Main {
         totalIncome = 0;
     }
 
-    public static void printSeats() {
+    public static void printCinema() {
         System.out.println("Cinema:");
         for (int i = 0; i < cinema.length; i++) {
             for (int j = 0; j < cinema[i].length; j++) {
@@ -47,31 +47,39 @@ public class Main {
     }
 
     public static void buyTicket() {
-        System.out.println("Enter a row number:");
-        int row = sc.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        int seat = sc.nextInt();
-
-        if (row > cinema.length - 1 || row < 0 || seat > cinema[0].length - 1 || seat < 0) {
-            System.out.println("Wrong input!");
-            buyTicket();
-        } else if (cinema[row][seat] == BUSY) {
-            System.out.println("That ticket has already been purchased!");
-            buyTicket();
-        } else {
-            if (rowCinema * seatCinema < MAX_SEATS || rowCinema / 2 > row) {
-                currentIncome += TICKET_NORMAL_PRICE;
-                System.out.printf("Ticket price: $%d\n", TICKET_NORMAL_PRICE);
-            } else {
-                currentIncome +=  TICKET_LOW_PRICE;
-                System.out.printf("Ticket price: $%d\n", TICKET_LOW_PRICE);
+        while (true) {
+            int row, seat;
+            try {
+                System.out.println("Enter a row number:");
+                row = sc.nextInt();
+                System.out.println("Enter a seat number in that row:");
+                seat = sc.nextInt();
+            } catch (InputMismatchException e) {
+                sc.nextLine();
+                System.out.println("You should enter numbers!");
+                continue;
             }
-            cinema[row][seat] = BUSY;
-            tickets++;
+
+            if (row > cinema.length - 1 || row < 0 || seat > cinema[0].length - 1 || seat < 0) {
+                System.out.println("Wrong input!");
+            } else if (cinema[row][seat] == BUSY) {
+                System.out.println("That ticket has already been purchased!");
+            } else {
+                if (rowCinema * seatCinema < MAX_SEATS || rowCinema / 2 >= row) {
+                    currentIncome += TICKET_NORMAL_PRICE;
+                    System.out.printf("Ticket price: $%d\n", TICKET_NORMAL_PRICE);
+                } else {
+                    currentIncome += TICKET_LOW_PRICE;
+                    System.out.printf("Ticket price: $%d\n", TICKET_LOW_PRICE);
+                }
+                cinema[row][seat] = BUSY;
+                tickets++;
+                break;
+            }
         }
     }
 
-    public static void statistics() {
+    public static void printStat() {
         if (rowCinema * seatCinema < MAX_SEATS) {
             totalIncome = rowCinema * seatCinema * TICKET_NORMAL_PRICE;
         } else {
@@ -86,17 +94,30 @@ public class Main {
     }
 
     public static void createCinema() {
-        System.out.println("Enter the number of rows:");
-        rowCinema = sc.nextInt();
-        System.out.println("Enter the number of seats in each row:");
-        seatCinema = sc.nextInt();
+        while (true) {
+            try {
+                System.out.println("Enter the number of rows:");
+                rowCinema = sc.nextInt();
+                System.out.println("Enter the number of seats in each row:");
+                seatCinema = sc.nextInt();
+
+                if (rowCinema < 0 || seatCinema < 0) {
+                    System.out.println("Wrong input!");
+                    continue;
+                }
+            } catch (Exception e) {
+                sc.nextLine();
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+            break;
+        }
         cinema = new char[rowCinema + 1][seatCinema + 1];
     }
 
     public static void menu() {
         createCinema();
-        statistics();
-        clearSeats();
+        clearCinema();
 
         while (true) {
             System.out.println("""
@@ -104,18 +125,22 @@ public class Main {
                     2. Buy a ticket
                     3. Statistics
                     4. Clear Seats
+                    5. New Cinema
                     0. Exit""");
 
             int num = sc.nextInt();
 
             if (num == 1) {
-                printSeats();
+                printCinema();
             } else if (num == 2) {
                 buyTicket();
             } else if (num == 3) {
-                statistics();
+                printStat();
             } else if (num == 4) {
-                clearSeats();
+                clearCinema();
+            } else if (num == 5) {
+                createCinema();
+                clearCinema();
             } else if (num == 0) {
                 break;
             }
