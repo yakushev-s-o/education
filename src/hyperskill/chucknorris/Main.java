841fe1ccd19bbf57554f7e3a0e3c2f8810c7cd94
Home <spirit40web@gmail.com>
2023-01-27 23:25:08 +0300

    update chucknorris/Main.java

--- src/hyperskill/chucknorris/Main.java
+++ src/hyperskill/chucknorris/Main.java
@@ -6,32 +6,62 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Input string:");
-        char[] arr = sc.nextLine().toCharArray();
-        StringBuilder binary = new StringBuilder();
+//        System.out.println("Input string:");
+        System.out.println("Input encoded string:");
+        String str = sc.nextLine();
 
         System.out.println("The result:");
-        for (char c : arr) {
-            binary.append(String.format("%07d", Integer.parseInt(Integer.toBinaryString(c))));
-        }
-
-        int i = 0;
-        char ch;
-
-        while (i < binary.length()) {
-            ch = binary.charAt(i);
-
-            System.out.print(binary.charAt(i) == '1' ? "0 " : "00 ");
-
-            while (binary.charAt(i) == ch) {
-                System.out.print("0");
-                i++;
-                if (i == binary.length()) break;
-            }
-
-            if (i < binary.length()) {
-                System.out.print(" ");
-            }
-        }
+//        encrypted(arr);
+        decrypted(str);
     }
+
+    private static void decrypted(String str) {
+        StringBuilder strDecrypted = new StringBuilder();
+        String[] chars = str.split(" ");
+        String first;
+        int second;
+
+        for (int i = 0; i < chars.length; i++) {
+            first = chars[i++].equals("0") ? "1" : "0";
+            second = chars[i].length();
+
+            while (second > 0) {
+                strDecrypted.append(first);
+                second--;
+            }
+        }
+
+        StringBuilder strToChar = new StringBuilder();
+        for (int i = 0; i < strDecrypted.length() / 7; i++) {
+            strToChar.append((char) (Integer.parseInt(strDecrypted.substring((7 * i), (i + 1) * 7),2)));
+        }
+        System.out.println(strToChar);
+    }
+
+//    private static void encrypted(String arr) {
+//        StringBuilder binary = new StringBuilder();
+//
+//        for (char c : arr.toCharArray()) {
+//            binary.append(String.format("%07d", Integer.parseInt(Integer.toBinaryString(c))));
+//        }
+//
+//        int i = 0;
+//        char ch;
+//
+//        while (i < binary.length()) {
+//            ch = binary.charAt(i);
+//
+//            System.out.print(binary.charAt(i) == '1' ? "0 " : "00 ");
+//
+//            while (binary.charAt(i) == ch) {
+//                System.out.print("0");
+//                i++;
+//                if (i == binary.length()) break;
+//            }
+//
+//            if (i < binary.length()) {
+//                System.out.print(" ");
+//            }
+//        }
+//    }
 }
