e509afd8861d558f2a9f4d814a7240f21e9fc273
Home <spirit40web@gmail.com>
2023-02-07 21:01:41 +0300

    update rockpaperscissors/Main.java

--- src/hyperskill/rockpaperscissors/Main.java
+++ src/hyperskill/rockpaperscissors/Main.java
@@ -1,22 +1,30 @@
 package hyperskill.rockpaperscissors;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String users = scanner.nextLine();
-        String computer;
+        String computer = switch (new Random().nextInt(3)) {
+            case 0 -> "scissors";
+            case 1 -> "rock";
+            default -> "paper";
+        };
 
-        if (users.equals("paper")) {
-            computer = "scissors";
-        } else if (users.equals("scissors")) {
-            computer = "rock";
+        checkWin(users, computer);
+    }
+
+    private static void checkWin(String users, String computer) {
+        if (users.equals(computer)) {
+            System.out.printf("There is a draw (%s)", computer);
+        } else if (users.equals("paper") && computer.equals("scissors") ||
+                users.equals("scissors") && computer.equals("rock") ||
+                users.equals("rock") && computer.equals("paper")) {
+            System.out.printf("Sorry, but the computer chose %s", computer);
         } else {
-            computer = "paper";
+            System.out.printf("Well done. The computer chose %s and failed", computer);
         }
-
-        System.out.println("Sorry, but the computer chose " + computer);
     }
 }
