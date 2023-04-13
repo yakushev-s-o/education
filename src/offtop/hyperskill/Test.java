83bd7049dc01fb54a6a8454c223f742f73cb53ff
Home <spirit40web@gmail.com>
2023-04-13 21:32:52 +0300

    update Test.java

--- src/offtop/hyperskill/Test.java
+++ src/offtop/hyperskill/Test.java
@@ -8,21 +8,46 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver_win32\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
 
         login(driver);
 
         driver.get("https://hyperskill.org/learn/step/2499");
+//        driver.get("http://91.217.76.232/learn/step/2499");
 
-        swapElements(driver);
+        checkElements(driver);
+//        swapElements(driver);
 
-//        driver.quit();
+        driver.quit();
+    }
+
+    private static void checkElements(WebDriver driver) {
+        if (checkDownload(driver, "//div[@class='submission']")) { // submission-correct
+            ArrayList<String> questions = new ArrayList<>();
+            ArrayList<String> answers = new ArrayList<>();
+
+            List<WebElement> count = driver.findElements(By.xpath("//div[@class='left-side__line']"));
+
+            for (int i = 1; i <= count.size(); i++) {
+                String question = "/html/body/div[1]/div[1]/div/div/div/div[4]/div/div/div[1]/div[1]/div/div[1]/div[" + i + "]/span";
+                String answer = "/html/body/div[1]/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div/div[2]/div/div[" + i + "]/div/span";
+                WebElement element1 = driver.findElement(By.xpath(question));
+                WebElement element2 = driver.findElement(By.xpath(answer));
+
+                String text1 = element1.getText();
+                String text2 = element2.getText();
+                questions.add(text1);
+                answers.add(text2);
+                System.out.println(questions.get(i - 1) + " --> " + answers.get(i - 1));
+            }
+        }
     }
 
     private static void swapElements(WebDriver driver) {
@@ -37,8 +62,8 @@ public class Test {
                 String text = element.getText();
 
                 if ("// a comment".equals(text)) {
-                   first = i;
-                   break;
+                    first = i;
+                    break;
                 }
             }
 
